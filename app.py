b84befb085483a72cb6606eebee7b11aadd98da5
Katherin Gomajoa <benavideskate07@gmail.com>
2026-02-24 11:52:48 -0500

    Instalacion de Flask y configuracion de la primera respuesta del servidor

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print("MI PRIMERA MESA DE TRABAJO DIGITAL")
+# Importamos la herramienta Flask para habilitar funciones de servidor web
+from flask import Flask
+
+#Creamos la aplicacion principal (instancia del motor web)
+# *__name__* indica que el servidor se ejecutara desde este archivo
+app = Flask(__name__)
+
+# Definimos la ruta raiz o página de inicio con el símbolo */*
+@app.route("/")
+def inicio():
+    # Esta función devuelve el mensaje que el ciudadano verá en su navegador
+    return "Bienvemido a la Plataforma Legal de Tutelas - Servidor Opertivo"
+
+# Bloque de seguridad para iniciar el servidor
+if __name__ == "__main__":
+    # Activamos el modo debug para detectar errores y refrescar cambios automaticamente
+    app.run(debug=True)
