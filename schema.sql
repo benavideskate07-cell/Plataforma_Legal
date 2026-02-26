2f98c9d7432b050cceed6d631796c8c61acc997f
Katherin Gomajoa <benavideskate07@gmail.com>
2026-02-26 08:04:39 -0500

    Configuracion inicial de la base de datos plataforma_legal.db

---
+++ schema.sql
@@ -0,0 +1,4 @@
+/* ARCHIVO DE ESQUEMA DE LA PLATAFORMA LEGAL
+Este archivo contendra las instrucciones para organizar los datos.
+Fase actual: Configuracion inicial del archivo de base de datos. */
+bh
