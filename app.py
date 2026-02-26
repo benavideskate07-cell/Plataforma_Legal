1be41d5675846a26b9a7858464fc0ff275e7b82c
Katherin Gomajoa <benavideskate07@gmail.com>
2026-02-26 08:57:05 -0500

    Evolucion de app.py: Implementacion de render_templates y conexion con index.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Importamos la herramienta Flask para habilitar funciones de servidor web
-from flask import Flask
+from flask import Flask,render_template
 
 #Creamos la aplicacion principal (instancia del motor web)
 # *__name__* indica que el servidor se ejecutara desde este archivo
@@ -8,8 +8,9 @@ app = Flask(__name__)
 # Definimos la ruta raiz o página de inicio con el símbolo */*
 @app.route("/")
 def inicio():
-    # Esta función devuelve el mensaje que el ciudadano verá en su navegador
-    return "Bienvemido a la Plataforma Legal de Tutelas - Servidor Opertivo"
+    # CAMBIO REALIZADO: En lugar de texto, devolvemos el archivo HTML
+    # Flask buscara "index.html" automaticamente dentro de la carpeta "templates"
+    return render_template("index.html")
 
 # Bloque de seguridad para iniciar el servidor
 if __name__ == "__main__":
