317a021fbe8f7393e1c9c660989debd478fae7bf
Katherin Gomajoa <benavideskate07@gmail.com>
2026-03-03 09:24:55 -0500

    Implementar arquitectura base.htnl: menu de visitante y footer

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-# Importamos la herramienta Flask para habilitar funciones de servidor web
+# =====================================================
+# Archivo: app.py
+# Proyecto: Legalis
+# Descripcion: Punto de entrada de la aplicacion Flask.
+# =====================================================
+
 from flask import Flask,render_template
 
-#Creamos la aplicacion principal (instancia del motor web)
-# *__name__* indica que el servidor se ejecutara desde este archivo
+#Inicializacion de la aplicacion
 app = Flask(__name__)
 
-# Definimos la ruta raiz o página de inicio con el símbolo */*
-@app.route("/")
-def inicio():
-    # CAMBIO REALIZADO: En lugar de texto, devolvemos el archivo HTML
-    # Flask buscara "index.html" automaticamente dentro de la carpeta "templates"
-    return render_template("index.html")
+@app.route("/") # Define una ruta web en Flask y la asocia a una funcion que atendera las peticiones a esa URL.
 
-# Bloque de seguridad para iniciar el servidor
+def inicio():
+    """Ruta principal del sistema Legalis.
+    Temporalmente renderiza la plantilla base para auditar 
+    menu y footer antes de integrar la tienda."""
+
+    return render_template("base.html")
+
 if __name__ == "__main__":
-    # Activamos el modo debug para detectar errores y refrescar cambios automaticamente
+# Arranque del servidor en modo desarrollo
     app.run(debug=True)
